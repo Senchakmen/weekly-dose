d1de5c3a9b2d8b6b4aeadb3992fce9acd428a6ce
andrii <senchakevuch@gmail.com>
2024-02-08 14:20:56 +0200

    Initial commit

---
+++ src/main/java/dev/asench/weeklydose/WeeklyDoseApplication.java
@@ -0,0 +1,13 @@
+package dev.asench.weeklydose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyDoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyDoseApplication.class, args);
+	}
+
+}
